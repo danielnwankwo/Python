01dac6c675d66bccbfc7def8f105d6023f397050
Daniel Nwankwo <chino@Daniels-MacBook-Air.local>
2020-11-02 15:05:49 +0000

    Python variables input and type

--- Python_Variables.py
+++ Python_Variables.py
@@ -1,1 +1,29 @@
-print("HelloWorld")
+# # How can we create a Variable?
+#
+# name = "Daniel" # Declaring string variable
+# # Creating a variable called name to store user name
+# age = 24 # integer
+# # Creating a variable called age to store user age
+# travel_allowance = 2.4 # float
+# # Creating a variable called travel_allowance .....
+# hourly_wage = 5 # integer
+# # Creating a variable called hourly_wage ......
+#
+# # Type of variable in Python
+# # String values, integer, float, boolean
+# print(name)
+# print(age)
+# print(travel_allowance)
+# print(hourly_wage)
+# How can we find out the type of variable
+# we have the type() method to find out the variable type in method
+# Print(type(variable name))
+# How can we take user data
+# We use a method called input() to get data from user
+
+name = input("PLease Enter Your Name")
+# Getting user data by using input method
+
+# Print(name)
+
+
--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 ### Pycharm 3.8
 ### Python Variable
 
+- What is a Variable 
+- Dynamically typed Languages
+- Overwriting Variables
+- Getting user input from the command line
+
+# Variable 
+- A place holder to store Data
+
+# Pseudo Coding 
+- Translation of your code
