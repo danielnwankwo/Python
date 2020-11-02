cf282497cde33a9038f34d9bee9722c1d05fed3a
Daniel Nwankwo <chino@Daniels-MacBook-Air.local>
2020-11-02 15:43:37 +0000

    Variable exercise

---
+++ varable exercise.py
@@ -0,0 +1,16 @@
+# Create 3 variables to get user data: name, DOB, age
+input("What is your name? ")
+input("What is your DOB? ")
+input("How old are you? ")
+
+name = input("What is your name ? ")
+DOB = input("What is your DOB in the format dd/mm/yyyy? ")
+age = input("How old are you ? ")
+
+Print(name)
+Print(DOB)
+Print(age)
+
+print(type(name))
+print(type(DOB))
+print(type(age))
